372103768f08e96875bd307427700d1122adec2f
Andy <andrew.rutherford.6@gmail.com>
2020-05-31 14:21:37 +0700

    create createUserProfile route

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -7,8 +7,8 @@ exports.getUserProfile = async (req, res, next) => {
   const userId = req.headers.user;
   console.log(userId);
   try {
-    const profile = await User.findOne({
-      user: userId,
+    const profile = await Profile.findOne({
+      _id: userId,
     });
 
     if (!profile) {
@@ -25,5 +25,42 @@ exports.getUserProfile = async (req, res, next) => {
   } catch (error) {
     console.error(err.message);
     res.status(500).send('Server Error');
+  }
+};
+
+// @desc  Create and update user profile
+// @route POST api/v1/profile
+exports.createUserProfile = async (req, res, nex) => {
+  const { location, cooler } = req.body;
+  const userId = req.headers.user;
+  let currentBeers;
+
+  try {
+    // Check beers in profile
+    let profile = await Profile.findOne({
+      user: userId,
+    });
+    currentBeers = profile.cooler;
+  } catch (error) {}
+
+  const profileFields = {
+    user: userId,
+    location,
+    cooler: [cooler, ...currentBeers],
+  };
+
+  try {
+    let profile = await Profile.findOneAndUpdate(
+      { user: userId },
+      { $set: profileFields },
+      { new: true, upsert: true }
+    );
+    res.json({
+      success: true,
+      profile,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
   }
 };
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getUserProfile } = require('../controllers/profile-controller');
+const { createUserProfile } = require('../controllers/profile-controller');
 
 router.route('/me').get(getUserProfile);
+router.route('/').post(createUserProfile);
 
 module.exports = router;
