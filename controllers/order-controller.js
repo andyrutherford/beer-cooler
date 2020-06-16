cbaa5bd5cfee4d9e4f1826ee191b6a71169f191b
Andy <andrew.rutherford.6@gmail.com>
2020-06-16 16:12:05 +0700

    created getOrderById route and controller

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,17 +1,21 @@
 const Order = require('../models/Order');
 const User = require('../models/User');
 
+const mongoose = require('mongoose');
+
 // @desc    Place new order
 // @route   POST /api/v1/orders/new
+// @access  PRIVATE
 exports.placeOrder = async (req, res, next) => {
   const userId = req.user.id;
-  const { address, payment } = req.body;
+  const { address, payment, cooler } = req.body;
   try {
     // Initialize empty profile for new user
     let order = new Order({
       user: userId,
       address: { ...address },
       payment: { ...payment },
+      cooler: [...cooler],
     });
     await order.save();
     console.log(order);
@@ -25,5 +29,39 @@ exports.placeOrder = async (req, res, next) => {
       success: false,
       error: 'A problem occurred.  Please try again',
     });
+  }
+};
+
+// @desc    Get order by order id
+// @route   GET /api/v1/orders/:id
+// @access  PRIVATE
+exports.getOrderById = async (req, res, next) => {
+  //   res.json({ orderID: req.params.order_id, user: req.user });
+  const orderId = req.params.order_id;
+  const userId = req.user.id;
+
+  //   Ensure the order ID is a valid object ID
+  if (!mongoose.isValidObjectId(orderId)) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Invalid order ID' });
+  }
+
+  try {
+    //   Ensure the order matches both the order ID and the ID of the user requesting it
+    const order = await Order.findOne({ _id: orderId, user: userId });
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: 'Order not found.',
+      });
+    }
+    res.json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
   }
 };
--- routes/order-route.js
+++ routes/order-route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { placeOrder } = require('../controllers/order-controller');
+const { placeOrder, getOrderById } = require('../controllers/order-controller');
 
 const authMiddleware = require('../middleware/auth');
 
 router.route('/new').post(authMiddleware, placeOrder);
+router.route('/:order_id').get(authMiddleware, getOrderById);
 
 module.exports = router;
