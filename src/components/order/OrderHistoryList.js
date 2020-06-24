489dd2e9f355589b890710f8e82798a6f3f27217
Andy <andrew.rutherford.6@gmail.com>
2020-06-24 11:23:40 +0700

    OrderHistoryList component shows number of items in each order

--- src/components/order/OrderHistoryList.js
+++ src/components/order/OrderHistoryList.js
@@ -5,6 +5,12 @@ import { Link } from 'react-router-dom';
 import { formatDate } from '../../utils/formatDate';
 
 export const OrderHistoryList = ({ history }) => {
+  const getQuantity = (item) => {
+    return item.cooler.reduce((acc, i) => {
+      return acc + i.quantity;
+    }, 0);
+  };
+
   return (
     <ul className='list-group list-group-flush'>
       {history.map((i) => (
@@ -12,10 +18,11 @@ export const OrderHistoryList = ({ history }) => {
           className='list-group-item d-flex justify-content-between align-items-center'
           key={i._id}
         >
-          {' '}
           <span>
-            <Link to={`/my-orders/${i._id}`}>Order #{i.orderId}</Link>
+            <Link to={`/my-orders/${i._id}`}>Order #{i.orderId}</Link> -{' '}
+            {getQuantity(i)} Items
           </span>
+
           <span className='text-muted'>
             <i className='far fa-calendar-alt'></i> {formatDate(i.date)}
           </span>
